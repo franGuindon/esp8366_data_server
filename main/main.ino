80e060cd6521c52133e43fa80239cb663508365c
franGuindon <fbadilla10@gmail.com>
2024-03-20 19:07:57 -0600

    Add initial blink test

---
+++ main/main.ino
@@ -0,0 +1,13 @@
+/* Copyright (c) 2024 Francis Guindon */
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+}
