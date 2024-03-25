98a16cb07f8be8cbafa191680b4ba1c30bb938e0
franGuindon <fbadilla10@gmail.com>
2024-03-24 19:58:11 -0600

    Set up logic to write data to ThingSpeak server

--- main/main.ino
+++ main/main.ino
@@ -1,68 +1,27 @@
-// #include <SoftwareSerial.h>
-
-// #include <ArduinoWiFiServer.h>
-// #include <BearSSLHelpers.h>
-// #include <CertStoreBearSSL.h>
-// #include <ESP8266WiFi.h>
-// #include <ESP8266WiFiAP.h>
-// #include <ESP8266WiFiGeneric.h>
-// #include <ESP8266WiFiGratuitous.h>
-// #include <ESP8266WiFiMulti.h>
-// #include <ESP8266WiFiSTA.h>
-// #include <ESP8266WiFiScan.h>
-// #include <ESP8266WiFiType.h>
-// #include <WiFiClient.h>
-// #include <WiFiClientSecure.h>
-// #include <WiFiClientSecureBearSSL.h>
-// #include <WiFiServer.h>
-// #include <WiFiServerSecure.h>
-// #include <WiFiServerSecureBearSSL.h>
-// #include <WiFiUdp.h>
-
-// #include <ESP8266AVRISP.h>
-// #include <command.h>
-
-// #include <dummy.h>
-
 /* Copyright (c) 2024 Francis Guindon */
 
-int delay_ms = 500;
-int bus_width;
-int data[] = {D0, D1, D2, D3, D4, D5, D6, D7, D8};
+#include "ThingSpeak.h"
+#include <ESP8266WiFi.h>
+
+char ssid[] = "El Arado";
+char pass[] = "ElArado1507";
+
+int status = WL_IDLE_STATUS;
+WiFiClient  client;
+
+int sensorValue;
+
+unsigned long myChannelNumber = 2483973;
+const char * myWriteAPIKey = "TMZANOXDNZ2QNZFP";
 
 void setup() {
-  Serial.begin(9600);
-  bus_width = sizeof(data)/sizeof(data[0]);
-  
-  for (int i = 0; i < bus_width; ++i) {
-    pinMode(data[i], OUTPUT);
-  }
-}
-
-void set_boometer_value(int value) {
-  for (int i = 0; i < value; ++i) {
-    digitalWrite(data[i], HIGH);
-  }
-
-  for (int i = value; i < 8; ++i) {
-    digitalWrite(data[i], LOW);
-  }
+  WiFi.begin(ssid, pass);
+  ThingSpeak.begin(client);
 }
 
 void loop() {
-  int sensorValue = analogRead(A0);
-  Serial.println(sensorValue);
-  delay(1);
+  sensorValue = analogRead(A0);
 
-  set_boometer_value(sensorValue >> 7);
-
-  // for (int i = 0; i < 8; ++i) {
-  //   digitalWrite(data[i], HIGH);
-  //   delay(delay_ms);
-  // }
-
-  // for (int i = 0; i < 8; ++i) {
-  //   digitalWrite(data[7-i], LOW);
-  //   delay(delay_ms);
-  // }
+  ThingSpeak.writeField(myChannelNumber, 1, sensorValue, myWriteAPIKey);
+  delay(5000);
 }
